531d798becc204bf8c24424ef6d94f7fc491d709
Jaakko Tähtinen <jaakko.tahtinen@gmail.com>
2022-11-07 01:41:18 +0200

    Cleanup

--- src/screenobject.cpp
+++ src/screenobject.cpp
@@ -4,9 +4,12 @@ bool initScreenObject(jadel::Vec2 pos, ScreenObject* target)
 {
     if (!target) return false;
     target->pos = pos;
-    target->screenRects.reserve(15);
-    target->screenSurfaces.reserve(15);
-    target->typeQueue.reserve(30);
+    //target->screenRects.reserve(15);
+    //target->screenSurfaces.reserve(15);
+    //target->typeQueue.reserve(30);
+    target->numElements = 0;
+    target->numRects = 0;
+    target->numSurfaces = 0;
     return true;
 }
 
@@ -19,42 +22,72 @@ ScreenRect createScreenRect(jadel::Vec2 pos, jadel::Vec2 dimensions, jadel::Colo
     return result;
 }
 
-ScreenSurface createScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, const jadel::Surface* surface)
+ScreenSurface createScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, jadel::Rectf sourceRect, const jadel::Surface* surface)
 {
     ScreenSurface result;
     result.pos = pos;
     result.dimensions = dimensions;
     result.surface = surface;
+    result.sourceRect = sourceRect;
     return result;
 }
 
-void screenObjectPushRect(ScreenRect rect, ScreenObject* target)
+ScreenSurface createScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, const jadel::Surface* surface)
 {
-    target->screenRects.emplace_back(rect);
-    target->typeQueue.emplace_back(RENDERABLE_TYPE_RECT);
+    ScreenSurface result = createScreenSurface(pos, dimensions, jadel::Rectf{0, 0, 1.0f, 1.0f}, surface);
+    return result;
 }
 
 void screenObjectPushScreenSurface(ScreenSurface surface, ScreenObject* target)
 {
-    target->screenSurfaces.emplace_back(surface);
-    target->typeQueue.emplace_back(RENDERABLE_TYPE_SURFACE);
+    if (!surface.surface) jadel::message("[WARNING] Tried to push null screen surface to screen object\n");
+    //target->screenSurfaces.emplace_back(surface);
+    //target->typeQueue.emplace_back(RENDERABLE_TYPE_SURFACE);
+    if (target->numSurfaces >= MAX_SCREEN_SURFACES_PER_OBJECT)
+    {
+        jadel::message("[ERROR] Too many surfaces in screen object!\n");
+        return;
+    }
+    target->screenSurfaces[target->numSurfaces++] = surface;
+    target->typeQueue[target->numElements++] = RENDERABLE_TYPE_SURFACE;
 }
 
-void screenObjectPushRect(jadel::Vec2 pos, jadel::Vec2 dimensions, jadel::Color color, ScreenObject* target)
+void screenObjectPushScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, const jadel::Surface* surface, jadel::Rectf sourceRect, ScreenObject* target)
 {
-    ScreenRect rect = {pos, dimensions, color};
-    screenObjectPushRect(rect, target);
+    ScreenSurface scrSurface = createScreenSurface(pos, dimensions, sourceRect, surface);
+    screenObjectPushScreenSurface(scrSurface, target);
 }
 
 void screenObjectPushScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, const jadel::Surface* surface, ScreenObject* target)
 {
-    ScreenSurface scrSurface = {pos, dimensions, surface};
-    screenObjectPushScreenSurface(scrSurface, target);
+    screenObjectPushScreenSurface(pos, dimensions, surface, jadel::Rectf{0, 0, 1.0f, 1.0f}, target);
+}
+
+void screenObjectPushRect(ScreenRect rect, ScreenObject* target)
+{
+    //target->screenRects.emplace_back(rect);
+    //target->typeQueue.emplace_back(RENDERABLE_TYPE_RECT);
+    if (target->numRects >= MAX_SCREEN_RECTS_PER_OBJECT)
+    {
+        jadel::message("[ERROR] Too many rects in screen object!\n");
+        return;
+    }
+    target->screenRects[target->numRects++] = rect;
+    target->typeQueue[target->numElements++] = RENDERABLE_TYPE_RECT;
+}
+
+void screenObjectPushRect(jadel::Vec2 pos, jadel::Vec2 dimensions, jadel::Color color, ScreenObject* target)
+{
+    ScreenRect rect = createScreenRect(pos, dimensions, color);
+    screenObjectPushRect(rect, target);
 }
 
 void screenObjectClear(ScreenObject* screenObject)
 {
-    screenObject->screenRects.clear();
-    screenObject->screenSurfaces.clear();
-    screenObject->typeQueue.clear();
+    //screenObject->screenRects.clear();
+    //screenObject->screenSurfaces.clear();
+    //screenObject->typeQueue.clear();
+    screenObject->numElements = 0;
+    screenObject->numRects = 0;
+    screenObject->numSurfaces = 0;
 }
--- src/screenobject.h
+++ src/screenobject.h
@@ -2,6 +2,9 @@
 #include <vector>
 #include <jadel.h>
 
+#define MAX_SCREEN_SURFACES_PER_OBJECT (500)
+#define MAX_SCREEN_RECTS_PER_OBJECT (500)
+
 enum RenderableType
 {
     RENDERABLE_TYPE_SURFACE = 0,
@@ -21,19 +24,28 @@ struct ScreenSurface
     jadel::Vec2 pos;
     jadel::Vec2 dimensions;
     const jadel::Surface* surface;
+    jadel::Rectf sourceRect;
 };
 
 struct ScreenObject
 {
     jadel::Vec2 pos;
-    std::vector<ScreenRect> screenRects;
+    /*std::vector<ScreenRect> screenRects;
     std::vector<ScreenSurface> screenSurfaces;
-    std::vector<uint32> typeQueue;
+    std::vector<uint32> typeQueue;*/
+    ScreenRect screenRects[MAX_SCREEN_RECTS_PER_OBJECT];
+    ScreenSurface screenSurfaces[MAX_SCREEN_SURFACES_PER_OBJECT];
+    uint32 typeQueue[MAX_SCREEN_RECTS_PER_OBJECT + MAX_SCREEN_SURFACES_PER_OBJECT];
+    size_t numElements;
+    size_t numRects;
+    size_t numSurfaces;
 };
 
 bool initScreenObject(jadel::Vec2 pos, ScreenObject* target);
 
 ScreenRect createScreenRect(jadel::Vec2 pos, jadel::Vec2 dimensions, jadel::Color color);
+
+ScreenSurface createScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, jadel::Rectf sourceRect, const jadel::Surface* surface);
 
 ScreenSurface createScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, const jadel::Surface* surface);
 
@@ -42,6 +54,8 @@ void screenObjectPushRect(ScreenRect rect, ScreenObject* target);
 void screenObjectPushScreenSurface(ScreenSurface surface, ScreenObject* target);
 
 void screenObjectPushRect(jadel::Vec2 pos, jadel::Vec2 dimensions, jadel::Color color, ScreenObject* target);
+
+void screenObjectPushScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, const jadel::Surface* surface, jadel::Rectf sourceRect, ScreenObject* target);
 
 void screenObjectPushScreenSurface(jadel::Vec2 pos, jadel::Vec2 dimensions, const jadel::Surface* surface, ScreenObject* target);
 
