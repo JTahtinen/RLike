025d87e2007156e24f94e86fff17ee80b5bf31c0
Jaakko Tähtinen <jaakko.tahtinen@gmail.com>
2022-11-18 01:51:15 +0200

    Moved assets to globals

---
+++ src/globals.cpp
@@ -0,0 +1,4 @@
+#include "globals.h"
+
+AssetCollection g_Assets;
+const Font* g_currentFont;
---
+++ src/globals.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "assetcollection.h"
+
+struct Font;
+
+extern AssetCollection g_Assets;
+extern const Font* g_currentFont;
